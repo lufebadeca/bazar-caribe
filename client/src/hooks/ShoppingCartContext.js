ea7c08ae7b56dbb8892786a36aa8f373a7d3db6b
lufebadeca <lufebadeca@gmail.com>
2025-05-02 16:39:10 -0500

    shopping cart context provider added

---
+++ client/src/hooks/ShoppingCartContext.js
@@ -0,0 +1,29 @@
+import { createContext, useContext, useState } from "react";
+
+const ShoppingCartContext = createContext(undefined);
+
+export const ShoppingCartProvider = ({children}) => {
+    const [cart, setCart] = useState([]);
+
+    const addToCart = (product) => {
+        setCart([...cart, product]);
+    }
+
+    const removeFromCart = (productId) => {
+        setCart(cart.filter((product) => product.id !== productId));
+    }
+
+    const clearCart = () => {
+        setCart([]);
+    }
+
+    return (
+        <ShoppingCartContext.Provider value={{ cart, addToCart, removeFromCart, clearCart }}>
+            {children}
+        </ShoppingCartContext.Provider>
+    )
+}
+
+export const useShoppingCart = () => {
+    return useContext(ShoppingCartContext);
+}
