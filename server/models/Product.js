0098c7348cdf59bf97c8c361feada97817f2b4bf
lufebadeca <lufebadeca@gmail.com>
2025-05-01 20:25:10 -0500

    schema modified to include spanish indexing

--- server/models/Product.js
+++ server/models/Product.js
@@ -51,7 +51,7 @@ const productSchema = new mongoose.Schema(
 );
 
 // Creamos un índice de texto para facilitar la búsqueda por título y descripción
-productSchema.index({ title: 'text', description: 'text', category: 'text' });
+productSchema.index({ title: 'text', description: 'text', category: 'text' }, { default_language: 'spanish' });
 
 const Product = mongoose.model('Product', productSchema);
 
